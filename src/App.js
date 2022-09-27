2dd42fdfaa41c04eb669b11493ff374b1ecac505
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2022-09-27 22:33:22 +0600

    react router added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar';
+import Home from './Pages/Home/Home';
+import Contact from './Pages/Contact/Contact'
+import AboutUs from './Pages/AboutUs/AboutUs';
+import Services from './Pages/Services/Services';
+import AllCountries from './Pages/AllCountries/AllCountries';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/aboutUs" element={<AboutUs/>} />
+        <Route path="/services" element={<Services/>} />
+        <Route path="/allCountries" element={<AllCountries/>} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/AllCountries/AllCountries.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllCountries = () => {
+    return (
+        <div>
+            here is al countries
+        </div>
+    );
+};
+
+export default AllCountries;
